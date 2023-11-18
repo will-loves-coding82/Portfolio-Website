53994eec51bc638466a495f518d068901aa16d2a
William Kim <wik002@sdsc.edu>
2023-11-17 23:32:18 -0800

    first commit

---
+++ assets/scripts/canvas.js
@@ -0,0 +1,10 @@
+
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    const canvas = document.querySelector("canvas");
+    const ctx = canvas.getContext("2d");
+    ctx.fillStyle = "lightgrey";
+    // Add a rectangle at (10, 10) with size 100x100 pixels
+    ctx.fillRect(0, 0, 400, 400);
+})
